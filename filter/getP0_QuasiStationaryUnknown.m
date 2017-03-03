f9eecc5563e9662a155dcb8fac5afb64fcfd2cef
Brandon <brandon.allen.wood@gmail.com>
2017-03-03 16:42:45 -0600

    feat: Initialize Covariance Function
    Input initial values for now.  May need to change

---
+++ filter/getP0_QuasiStationaryUnknown.m
@@ -0,0 +1,15 @@
+function getP0_QuasiStationaryUnknown()
+
+sigma_r = [0.0162,0.0144,2]; %Taken as the difference between champaign and campus lat/long/elevation
+sigma_v = [10^-6,10^-6,10^-6]; % initial guess
+sigma_t = [pi/2, 0.0698,0.0698];  %4 degree uncertainty
+sigma_cosSi = 1; % initial guess
+sigma_sinSi = 1; % initial guess
+sigma_ba = [0.1,0.1,0.1]; % initial guess
+sigma_bg = [0.1,0.1,0.1]; % initial guess
+
+
+
+
+
+end
