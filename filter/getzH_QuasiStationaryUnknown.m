2c3f2653ebc6eca49903402bbdf5bdc60a1248ad
Brandon <brandon.allen.wood@gmail.com>
2017-03-05 19:51:23 -0600

    fix: Typo in H matrix

--- filter/getzH_QuasiStationaryUnknown.m
+++ filter/getzH_QuasiStationaryUnknown.m
@@ -1,10 +1,24 @@
 function [dz,H] = getzH_QuasiStationaryUnknown(state)
+%getzH_QuasiStationaryUnknown : get measurement and measurement mapping
+%matix for the Quasi-stationary unknown heading implementation
+%Inputs
+%   state : data structure containing elements of the state
+%
+%Outputs
+%   dz : measurement innofvation dz = -Delta(r)
+%   H : measurement mapping matrix
+%
+%Reference
+%   Equations from Chapter 13 : Inertial Navigation pg 415
+%
+%Log 
+% 3/5/17 Brandon Wood : Initial Implementation
 
 O = zeros(3,3);
 o = zeros(3,1);
 I = eye(3);
 
 dz = -state.dr;
-H = [ O O -I o o O O];
+H = [ -I O O o o O O];
 
 end
--- filter/getzH_QuasiStationaryZVU.m
+++ filter/getzH_QuasiStationaryZVU.m
@@ -1,9 +1,22 @@
 function [dz,H] = getzH_QuasiStationaryZVU(state)
-
+%getzH_QuasiStationaryZVU : get measurement and measurement mapping
+%matix for the Quasi-stationary zero velocity update implementation
+%Inputs
+%   state : data structure containing elements of the state
+%
+%Outputs
+%   dz : measurement innovation 
+%   H : measurement mapping matrix
+%
+%Reference
+%   Equations from Chapter 13 : Inertial Navigation pg 416
+%
+%Log 
+% 3/5/17 Brandon Wood : Initial Implementation
 O = zeros(3,3);
 I = eye(3);
 
 dz = state.r0-state.r;
-H = [ O O -I O O];
+H = [ -I O O O O];
 
 end
