252cf4e5296792fbc1c9f62379cb364c615b84ec
Brandon <brandon.allen.wood@gmail.com>
2017-03-08 15:16:51 -0600

    fix: Initial Bugs from testing

--- quasiStationaryAlignUnknownHeading.m
+++ quasiStationaryAlignUnknownHeading.m
@@ -3,15 +3,20 @@ function state = quasiStationaryAlignUnknownHeading(state,params,imu)
 %Propogate state with IMU measurements
 state = imuStatePropogationUnknown(state,params,imu);
 
+
 %Error and uncertainty propogation
 F = getF_QuasiStationaryUnknown(state,params,imu); % Get State Jacobian
-PHI = getPHI_QuasiStationaryUnknown(state,F); % Get state transition matrix
+PHI = getPHI_QuasiStationaryUnknown(state,imu,F); % Get state transition matrix
 %state.dx = PHI*state.dx; % Since states are zeroed, no need to propogate
-Pk = PHI*state.P*PHI' + params.Q*params.dt; % Propogate uncertainty
+Pk = PHI*state.P*PHI' + params.Q*imu.dt; % Propogate uncertainty
+
+if params.loopCount>100
+    keyboard;
+end
 
 %Compute Kalman Gain
 [dz,H] = getzH_QuasiStationaryUnknown(state); % measurement and mapping
-Gamma = H*P*H' + params.R; % Innovations
+Gamma = H*Pk*H' + params.R; % Innovations
 K = Pk*H'/Gamma;
 
 
@@ -24,14 +29,19 @@ state.dx = state.dx + K*dz;
 state.dr = state.dr - state.dx(1:3);
 state.v = state.v - state.dx(4:6);
 state.t = state.t - state.dx(7:9);
-state.T = T_ZYX_Unknown(state.dx(10),state.dx(11),state.dx(8),state.dx(9))'*state.T; % Errors may not yet be <2deg
 state.sinSi = state.sinSi - state.dx(10);
 state.cosSi = state.cosSi - state.dx(11);
+state.T = R_ZYX(state.dx(7),state.dx(8),state.dx(9))'*state.T; % Errors may not yet be <2deg
+%state.T = T_ZYX_Unknown(state.sinSi,state.cosSi,state.dx(8),state.dx(9))'*state.T; % Errors may not yet be <2deg
+%state.T = R_ZYX(state.t(1),state.t(2),state.t(3));
+%dT = [state.dx(11), state.dx(10), 0.0; -state.dx(10), state.dx(11), 0.0; 0.0,0.0,1.0]*(eye(3) + skewmat([state.dx(9),state.dx(8),0]));
+%state.T = dT*state.T;
+state.T = reOrthoNorm(state.T);
 state.b_a = state.b_a - state.dx(12:14);
 state.b_g = state.b_g - state.dx(15:17);
 
 %Zero Kalman Filter Estimate of position, velocity, attitude ( see pg 366)
-state.dx(1:9) = zeros(9,1);
+state.dx(1:end) = zeros(state.n,1);
 
 
 end
